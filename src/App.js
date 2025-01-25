117b2ca58cb48b4254bb38e80ce675ead5230129
Amos omohodion <amosomohodion@gmail.com>
2025-01-25 13:56:12 +0100

    added new features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 // import { jwtDecode } from 'jwt-decode'
 // import { ToastContainer } from 'react-toastify'
 // import 'react-toastify/dist/ReactToastify.css'
-import HomePage from './pages/Homepage'
-import SignInPage from './pages/SignIn'
-import SignUpPage from './pages/SignUp'
-import Navbar from './pages/test'
+import HomePage from "./pages/Homepage";
+import SignInPage from "./pages/SignIn";
+import SignUpPage from "./pages/SignUp";
+import Navbar from "./pages/test";
+import ForgotPasswordPage from "./pages/ForgotPassword";
+import TokenVerification from "./pages/TokenVerificationPage";
+import ResetPassword from "./pages/ResetPasswordPage";
 
 function App() {
   return (
-   <>
-   <Routes>
-   <Route path='/' element={<HomePage />} />
-   <Route path='/sign-in' element={<SignInPage />} />
-   <Route path='/sign-up' element={<SignUpPage />} />
-   <Route path='/test' element={<Navbar/>} />
-   </Routes>
-   </>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/token/:email" element={<TokenVerification />} />
+        <Route path="/reset-password/:email" element={<ResetPassword />} />
+        <Route path="/test" element={<Navbar />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
