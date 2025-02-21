e9c773479fea421c5d9e7e7d0f81ff8f585284bf
Amos omohodion <amosomohodion@gmail.com>
2025-02-21 16:59:11 +0100

    deployment process

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({baseUrl: '/api/v1', 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+const baseQuery = fetchBaseQuery({baseUrl: `${API_BASE_URL}/api/v1`, 
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('userInfo');
         const parsedToken = JSON.parse(token);
